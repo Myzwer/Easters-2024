088383c7c05ae5dd9bb6b8264a78caba26fcc3a1
Myzwer <josh@onefortyfivedesign.com>
2024-02-22 11:08:49 -0500

    :white_check_mark: Added 2 new template files for controlling pages on the site.

---
+++ ftg.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Template Name: Custom - First Time Guest
+ *
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package WordPress
+ * @subpackage BunnyFooFoo
+ * @since 1.0.0
+ */
+
+get_header(); ?>
+
+<!-- The Content-->
+
+<?php get_footer();
---
+++ good-friday.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Template Name: Custom - Good Friday
+ *
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package WordPress
+ * @subpackage BunnyFooFoo
+ * @since 1.0.0
+ */
+
+get_header(); ?>
+
+    <!-- The Content-->
+
+<?php get_footer();
