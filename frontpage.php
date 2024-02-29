eae8f7f22bb0c8831b7baea84b441e9e21be27b5
Myzwer <josh@onefortyfivedesign.com>
2024-02-29 15:19:57 -0500

    :rocket: Updated frontpage layout and dynamic content
    The frontpage.php file has been modified to include dynamic content such as the background video, main brand, and main call-to-action (CTA). This will enable the admin to easily modify these components via the backend. The layout has been updated to match the mockup

--- frontpage.php
+++ frontpage.php
@@ -14,38 +14,57 @@
 
 get_header(); ?>
 
-    <video class="header-video" src="https://foothillscollective.com/wp-content/uploads/2021/04/Res-Power-Background.mp4" autoplay loop playsinline muted></video>
+    <video class="header-video" src="<?php the_field('background_video'); ?>" autoplay loop playsinline muted></video>
 
     <div class="viewport-header">
         <div class="head-container">
-            <div class="center add-padding">
-                <h1 class="text-white text-5xl pb-5">Header Title</h1>
+            <img class="w-11/12 md:w-8/12 lg:w-1/3 max-w-8xl mx-auto" src="<?php the_field('main_brand'); ?>" alt="">
+
+            <div class="pt-5">
+                <?php if (have_rows('main_cta')): ?>
+                    <?php while (have_rows('main_cta')): the_row();
+                        ?>
+                        <a href="<?php the_sub_field('button_link'); ?>">
+                            <button class="fab-main">
+                                <?php the_sub_field('button_text'); ?>
+                            </button>
+                        </a>
+                    <?php endwhile; ?>
+                <?php endif; ?>
             </div>
-            <hr class="text-white pb-5">
-            <h2 class="text-white text-3xl ">Title</h2>
-            <h3 class="text-white text-2xl">Subtitle</h3>
         </div>
     </div>
+
+    <div class="bg-gray text-white p-10">
+        <div class="text-center mx-auto">
+            <h1 class = "uppercase font-bold text-2xl lg:text-5xl"><?php the_field('tagline'); ?></h1>
+        </div>
+    </div>
+
 
     <div class="bg-white md:p-10">
         <div class=" lg:max-w-5xl lg:mx-auto">
             <div class="grid grid-cols-12 gap-4 md:gap-10">
                 <div class="col-span-12 md:col-span-4">
-                    <?php get_template_part( 'components/cards/_sample-card' ); ?>
+                    <?php get_template_part('components/cards/_sample-card'); ?>
                 </div>
 
                 <div class="col-span-12 md:col-span-4">
-                    <div class="prose">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid architecto error et laudantium molestiae odit tempora temporibus? Aliquid aspernatur aut autem dolore, enim minima molestias sapiente. Consectetur culpa nihil obcaecati?</div>
+                    <div class="prose">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid architecto
+                        error et laudantium molestiae odit tempora temporibus? Aliquid aspernatur aut autem dolore, enim
+                        minima molestias sapiente. Consectetur culpa nihil obcaecati?
+                    </div>
                 </div>
 
                 <div class="col-span-12 md:col-span-4 prose">
                     <h2>Header Line</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores earum magni nihil praesentium quam sequi sunt vitae. Excepturi iure molestias perferendis quidem, recusandae tempora. A hic nihil pariatur suscipit ut.</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores earum magni nihil
+                        praesentium quam sequi sunt vitae. Excepturi iure molestias perferendis quidem, recusandae
+                        tempora. A hic nihil pariatur suscipit ut.</p>
                 </div>
             </div>
         </div>
     </div>
-
 
 
 <?php
