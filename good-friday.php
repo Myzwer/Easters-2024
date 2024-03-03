1ac5aa19dfe627919abc9524ded71efda6472665
Myzwer <josh@onefortyfivedesign.com>
2024-03-02 23:17:59 -0500

    :rocket: Created the good friday page like the mockup shows.

--- good-friday.php
+++ good-friday.php
@@ -12,6 +12,40 @@
 
 get_header(); ?>
 
-    <!-- The Content-->
+    <div class="bg-gray">
+        <div class="hidden xl:block absolute right-0">
+            <img class="opacity-30" src="<?php the_field('bread', 'options'); ?>" alt="Bread and Juice">
+        </div>
+        <div class=" lg:max-w-5xl lg:mx-auto py-28 px-5">
+            <h1 class="text-8xl md:text-9xl font-bold mb-5 text-white"><?php the_field('title'); ?></h1>
+            <h3 class="text-xl mb-5 text-white"><?php the_field('details'); ?></h3>
+        </div>
+    </div>
+
+<?php
+// Check value exists.
+if (have_rows('experience_info')) :
+
+    // Loop through rows.
+    while (have_rows('experience_info')) : the_row();
+
+        switch (get_row_layout()) {
+            case 'dark_mode_block':
+                get_template_part('components/blocks/_dark-block');
+                break;
+
+            case 'light_mode_block':
+                get_template_part('components/blocks/_light-block');
+                break;
+
+        }
+
+        // End loop.
+    endwhile;
+endif;
+?>
+
+
+<?php get_template_part('components/blocks/_faq-light'); ?>
 
 <?php get_footer();
